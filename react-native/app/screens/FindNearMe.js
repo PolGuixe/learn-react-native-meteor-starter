6e6026a278f28e0d9aca1d6ba445d807d16bb229
PolGuixe <pol@zetoff.com>
2017-04-04 10:12:36 -0400

    Added geolocation

--- react-native/app/screens/FindNearMe.js
+++ react-native/app/screens/FindNearMe.js
@@ -1,6 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Container from '../components/Container';
 import { Header } from '../components/Text';
+import LocateMeButton from '../components/LocateMeButton';
+import config from '../config/config';
+
+
+const geoSettings = {
+  enableHighAccuracy: true,
+  timeout: 20000,
+  maximumAge: 1000,
+};
+
 
 class FindNearMe extends Component {
   static route = {
@@ -9,9 +19,33 @@ class FindNearMe extends Component {
     },
   }
 
+  static propTypes = {
+    navigator: PropTypes.object,
+  }
+
+  handleGeolocationSuccess = (position) => {
+    console.log('latitude', position.coords.latitude);
+    console.log('longitude', position.coords.longitude);
+  };
+
+  handleGeolocationError = (error) => {
+    this.props.navigator.showLocalAlert(error.message, config.errorStyles);
+  };
+
+  goToNearMe = () => {
+    navigator.geolocation.getCurrentPosition(
+      this.handleGeolocationSuccess,
+      this.handleGeolocationError,
+      geoSettings,
+    );
+  }
+
   render() {
     return (
       <Container>
+        <LocateMeButton
+          onPress={() => this.goToNearMe()}
+        />
         <Header>
           Find Near Me
         </Header>
