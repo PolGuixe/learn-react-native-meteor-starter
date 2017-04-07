76901492d5b5fe4688002e7d0d4fc3b65e2723a7
PolGuixe <pol@zetoff.com>
2017-04-06 22:51:41 -0400

    Fixed previous comment

--- meteor/imports/startup/server/accounts.js
+++ meteor/imports/startup/server/accounts.js
@@ -1,6 +1,6 @@
 import { Accounts } from 'meteor/accounts-base';
 
-Accounts.onCreatedUser(function onCreatedUser(options, user) {
+Accounts.onCreateUser(function onCreatedUser(options, user) {
   if (!user.username) {
     user.username = user.mail.split('@')[0]; //eslint-disable-line
   }
