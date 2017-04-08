a6bee3bde45759e33b9a1fdfa3f7b960d8cf6edf
PolGuixe <pol@zetoff.com>
2017-04-07 20:32:26 -0400

    Added signIn screen UI

--- react-native/app/screens/SignIn.js
+++ react-native/app/screens/SignIn.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { Card } from 'react-native-elements';
+import Router from '../config/router';
 import Container from '../components/Container';
-import { Header } from '../components/Text';
+import { Input, PrimaryButton, SecondaryButton } from '../components/Form';
 
 class SignIn extends Component {
   static route = {
@@ -8,14 +10,46 @@ class SignIn extends Component {
       visible: true,
       title: 'Sign In',
     },
+  };
+
+  static propTypes = {
+    navigator: PropTypes.object,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      emailOrUsername: '',
+      password: '',
+    };
   }
+
+  goToSignUp = () => {
+    this.props.navigator.pop();
+  };
 
   render() {
     return (
-      <Container>
-        <Header>
-          Sign In
-        </Header>
+      <Container scroll>
+        <Card>
+          <Input
+            label="Email or username"
+            placeholder="Please enter your email or username ..."
+            keyboardType="email-address"
+            value={this.state.emailOrUsername}
+            onChangeText={emailOrUsername => this.setState({ emailOrUsername })}
+          />
+          <Input
+            label="Password"
+            placeholder="Please enter your password ..."
+            secureTextEntry
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+          <PrimaryButton title="Sign In" />
+        </Card>
+        <SecondaryButton title="Sign Up" onPress={this.goToSignUp} />
       </Container>
     );
   }
