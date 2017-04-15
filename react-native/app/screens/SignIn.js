9e457b9a2a5d925f74c8258adcdd3cc5ea8d85f9
PolGuixe <pol@zetoff.com>
2017-04-15 19:06:38 +0200

    Implemented sign in

--- react-native/app/screens/SignIn.js
+++ react-native/app/screens/SignIn.js
@@ -1,8 +1,10 @@
 import React, { Component, PropTypes } from 'react';
+import Meteor from 'react-native-meteor';
 import { Card } from 'react-native-elements';
 import Router from '../config/router';
 import Container from '../components/Container';
 import { Input, PrimaryButton, SecondaryButton } from '../components/Form';
+import config from '../config/config';
 
 class SignIn extends Component {
   static route = {
@@ -22,11 +24,38 @@ class SignIn extends Component {
     this.state = {
       emailOrUsername: '',
       password: '',
+      signingIn: false,
     };
   }
 
   goToSignUp = () => {
-    this.props.navigator.pop();
+    this.props.navigator.push();
+  };
+
+  signIn = () => {
+    const { emailOrUsername, password } = this.state;
+
+    if (emailOrUsername.length === 0) {
+      return this.props.navigator.showLocalAlert(
+        'You need to provide a username or email to sign in',
+        config.errorStyles,
+      );
+    }
+    if (password.length === 0) {
+      return this.props.navigator.showLocalAlert(
+        'Your password should not be empty',
+        config.errorStyles,
+      );
+    }
+    this.setState({ signingIn: true });
+    return Meteor.loginWithPassword(emailOrUsername, password, error => {
+      this.setState({ signingIn: false });
+      if (error) {
+        this.props.navigator.showLocalAlert(error.reason, config.errorStyles);
+      } else {
+        this.props.navigator.immediatelyResetStack([Router.getRoute('profile')]);
+      }
+    });
   };
 
   render() {
@@ -47,7 +76,7 @@ class SignIn extends Component {
             value={this.state.password}
             onChangeText={password => this.setState({ password })}
           />
-          <PrimaryButton title="Sign In" />
+          <PrimaryButton title="Sign In" onPress={this.signIn} loading={this.state.signingIn} />
         </Card>
         <SecondaryButton title="Sign Up" onPress={this.goToSignUp} />
       </Container>
